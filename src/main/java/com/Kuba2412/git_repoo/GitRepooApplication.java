51dbed3341027ca3ebea7ef1ee474298a798de2c
Kuba Poswiatowski <xkubusx5@gmail.com>
2024-07-19 18:47:15 +0200

    add project

---
+++ src/main/java/com/Kuba2412/git_repoo/GitRepooApplication.java
@@ -0,0 +1,13 @@
+package com.Kuba2412.git_repoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kuba2412/git_repoo/GitRepooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kuba2412.git_repoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRepooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
